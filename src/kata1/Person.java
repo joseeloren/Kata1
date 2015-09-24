e3447a21eb2bd5f7f047595642bc80474ab0cde8
usuario <usuario@10.22.143.9>
2015-09-24 09:58:09 +0100

    Segunda version

--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -11,9 +11,12 @@ import java.util.Date;
  * @author usuario
  */
 public class Person {
+
     private final String name;
     private final String surname;
     private final Date birth;
+    private static final long MILLISECONDS_PER_YEAR =
+            (long) (1000 * 60 * 60 * 24 * 365.25);
 
     public Person(String name, String surname, Date birth) {
         this.name = name;
@@ -32,14 +35,18 @@ public class Person {
     public Date getBirth() {
         return birth;
     }
-    
+
     public String getFullName() {
         return name + " " + surname;
     }
-    
+
     public int getAge() {
         Date today = new Date();
-       
-        return (int) ((today.getTime() - birth.getTime())/(1000*60*60*24*365.25));
+
+        return (int) milliseconds(today.getTime() - birth.getTime());
+    }
+
+    private long milliseconds(long millis) {
+        return millis / MILLISECONDS_PER_YEAR;
     }
 }
